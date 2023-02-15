f9bb96b5493d879d7bc1677910279f1ddea70e1f
Fernando Blanco <fernandoblancovaldez@gmail.com>
2023-02-15 16:34:45 -0300

    4th try to make functiona tvsearcher

--- src/components/TvSearcher.js
+++ src/components/TvSearcher.js
@@ -37,7 +37,7 @@ const TvSearcher = () => {
         /* console.log(newSearchedShows); */
       });
       setShows(newSearchedShows);
-      console.log(shows);
+      /* console.log(shows); */
     };
     getShows();
     setLoading(false);
