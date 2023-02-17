a8406105c679b5945a2bfb478be4d47ff720d5dc
Fernando Blanco <fernandoblancovaldez@gmail.com>
2023-02-17 03:58:19 -0300

    cleaning code to netlify build production

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,10 +2,8 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { useFetch } from "../hooks/useFetch";
 
 const Weather = () => {
-  useFetch();
   return (
     <Card className="glass-dark text-secondary border-glass shadow py-3 gap-3 text-center align-items-center max-w-weather mx-auto">
       <Row>
