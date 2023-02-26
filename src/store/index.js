c060b45c222bfc496b213e6c80816231b1a2a84b
Fernando Blanco <fernandoblancovaldez@gmail.com>
2023-02-26 15:24:59 -0300

    changing redux function legacy_createStore as createStore to run online

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
 import reducer from "../reducers";
 
 const store = createStore(reducer);
 
-store.subscribe();
+store.subscribe(() => console.log(store));
 
 export default store;
